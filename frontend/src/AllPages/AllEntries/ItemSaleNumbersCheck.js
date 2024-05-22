04cdba672bf57124e964789d5a065cce6aaf69e7
Rahul <rahulsoni@gmail.com>
2024-05-22 18:29:31 +0530

    22/05/24 again

--- frontend/src/AllPages/AllEntries/ItemSaleNumbersCheck.js
+++ frontend/src/AllPages/AllEntries/ItemSaleNumbersCheck.js
@@ -95,6 +95,37 @@ const ItemSaleNumbersCheck = () => {
 
                 </div>
                 <div className='row'>
+                <div className='col-4 py-2'>
+                        <h6 className='text-center bg-dark text-white py-2 mb-0'>Item's Purchase Detail</h6>
+                        <table className="table table-success table-striped mb-0" >
+                            <thead className='sticky-top'>
+                                <tr>
+                                    <th scope="col" className='border-end border-dark'>#</th>
+                                    <th scope="col" className='border-end border-dark'>Item Name</th>
+                                    <th scope="col" className='border-end border-dark'>Total Quantity Of Item Purchase</th>
+                                    <th scope="col" className='border-end border-dark'>Total Amount Of Item Purchase</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                                {itemData.length > 0 ?
+                                    itemData.filter((item) => item.category === 'good').map((data, index) => {
+                                        SalesNumbers = purchaseBillData ? purchaseBillData.length > 0 ? itemPurchaseNumbers(data.itemname) : '' : ''
+                                        return (
+                                            <tr key={data._id}  >
+                                                <td className='border-end border-dark'>{index + 1}</td>
+                                                <td className='border-end border-dark'>{data.itemname}</td>
+                                                <td className='border-end border-dark'>{SalesNumbers.TQuantity}</td>
+                                                <td className='border-end border-dark'>{SalesNumbers.TAmount}</td>
+                                            </tr>
+                                        )
+
+                                    })
+
+                                    : <tr><td colSpan={4} className='text-center'>Data Will Display Here </td></tr>}
+                            </tbody>
+                        </table>
+                    </div>
                     <div className='col-4 py-2'>
                         <h6 className='text-center bg-dark text-white py-2 mb-0'>Item's Sale Detail</h6>
                         <table className="table table-success table-striped mb-0" >
@@ -155,37 +186,7 @@ const ItemSaleNumbersCheck = () => {
                             </tbody>
                         </table>
                     </div>
-                    <div className='col-4 py-2'>
-                        <h6 className='text-center bg-dark text-white py-2 mb-0'>Item's Purchase Detail</h6>
-                        <table className="table table-success table-striped mb-0" >
-                            <thead className='sticky-top'>
-                                <tr>
-                                    <th scope="col" className='border-end border-dark'>#</th>
-                                    <th scope="col" className='border-end border-dark'>Item Name</th>
-                                    <th scope="col" className='border-end border-dark'>Total Quantity Of Item Purchase</th>
-                                    <th scope="col" className='border-end border-dark'>Total Amount Of Item Purchase</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-
-                                {itemData.length > 0 ?
-                                    itemData.filter((item) => item.category === 'good').map((data, index) => {
-                                        SalesNumbers = purchaseBillData ? purchaseBillData.length > 0 ? itemPurchaseNumbers(data.itemname) : '' : ''
-                                        return (
-                                            <tr key={data._id}  >
-                                                <td className='border-end border-dark'>{index + 1}</td>
-                                                <td className='border-end border-dark'>{data.itemname}</td>
-                                                <td className='border-end border-dark'>{SalesNumbers.TQuantity}</td>
-                                                <td className='border-end border-dark'>{SalesNumbers.TAmount}</td>
-                                            </tr>
-                                        )
-
-                                    })
-
-                                    : <tr><td colSpan={4} className='text-center'>Data Will Display Here </td></tr>}
-                            </tbody>
-                        </table>
-                    </div>
+                   
 
                    
                 </div>
